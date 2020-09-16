59ce29d476883cad21a9f959af07f35450932ba4
Андрей В. Карачун <karachun_a@sportedu.by>
2020-09-16 13:45:16 +0300

    Initial commit

---
+++ app/src/main/java/com/andrewKarachun0304/tmshomeworkandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.andrewKarachun0304.tmshomeworkandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
