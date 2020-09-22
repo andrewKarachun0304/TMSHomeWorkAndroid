2758e7c94493092502a1a6fad78dd703dc681177
andrewKarachun0304 <andrusha00765@gmail.com>
2020-09-22 14:18:10 +0300

    Home work number 1

--- app/src/main/java/com/andrewKarachun0304/tmshomeworkandroid/MainActivity.kt
+++ app/src/main/java/com/andrewKarachun0304/tmshomeworkandroid/MainActivity.kt
@@ -4,15 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun onHw1BtnClick(view: View) {
-        val intent = Intent(this, HomeWork1Activity::class.java)
-        startActivity(intent)
+        hw1_btn.setOnClickListener {
+            val intent = Intent(this, HomeWork1Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
