d140f01f193b7ae1dc8f285bb3a305f024753655
Andrew <35775523+andrewKarachun0304@users.noreply.github.com>
2020-09-22 13:55:51 +0300

    Revert "Loading image by url"

--- app/src/main/java/com/andrewKarachun0304/tmshomeworkandroid/HomeWork1Activity.kt
+++ app/src/main/java/com/andrewKarachun0304/tmshomeworkandroid/HomeWork1Activity.kt
@@ -2,26 +2,10 @@ package com.andrewKarachun0304.tmshomeworkandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_home_work1.*
 
 class HomeWork1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_work1)
-    }
-
-    fun loadImageOnClick(view: View) {
-        if (imageUrlET.text.isEmpty()) {
-            Toast.makeText(this, "Please, enter image URL", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        Picasso
-            .get()
-            .load(imageUrlET.text.toString())
-            .into(loadImageIV)
     }
 }
