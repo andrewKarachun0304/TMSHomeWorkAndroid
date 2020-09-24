1305b446aad75acb7b33956e921a785b349637d0
andrewKarachun0304 <andrusha00765@gmail.com>
2020-09-24 12:06:13 +0300

    Home work 2. Add FlagActivity and AnimationActivity

---
+++ app/src/main/java/com/andrewKarachun0304/tmshomeworkandroid/FlagsActivity.kt
@@ -0,0 +1,11 @@
+package com.andrewKarachun0304.tmshomeworkandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlagsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flags)
+    }
+}
--- app/src/main/java/com/andrewKarachun0304/tmshomeworkandroid/MainActivity.kt
+++ app/src/main/java/com/andrewKarachun0304/tmshomeworkandroid/MainActivity.kt
@@ -3,16 +3,21 @@ package com.andrewKarachun0304.tmshomeworkandroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        hw1_btn.setOnClickListener {
-            val intent = Intent(this, HomeWork1Activity::class.java)
-            startActivity(intent)
+    fun homeWorkBtnOnClick(view: View) {
+        val myIntent: Intent? = when(view.id){
+            R.id.hw1_btn -> Intent(this, HomeWork1Activity::class.java)
+            R.id.hw2_btn -> Intent(this, HomeWork2Activity::class.java)
+            else -> Intent(this, MainActivity::class.java)
         }
+        startActivity(myIntent)
     }
 }
